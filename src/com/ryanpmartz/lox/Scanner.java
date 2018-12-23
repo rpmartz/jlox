45f12d22bb37b510a705d32ac04ec0e1c8ce48a1
Ryan Martz <martzrp@gmail.com>
2018-12-23 09:01:19 -0500

    implement scan token method

--- src/com/ryanpmartz/lox/Scanner.java
+++ src/com/ryanpmartz/lox/Scanner.java
@@ -1,6 +1,16 @@
 package com.ryanpmartz.lox;
 
+import static com.ryanpmartz.lox.TokenType.COMMA;
+import static com.ryanpmartz.lox.TokenType.DOT;
 import static com.ryanpmartz.lox.TokenType.EOF;
+import static com.ryanpmartz.lox.TokenType.LEFT_BRACE;
+import static com.ryanpmartz.lox.TokenType.LEFT_PAREN;
+import static com.ryanpmartz.lox.TokenType.MINUS;
+import static com.ryanpmartz.lox.TokenType.PLUS;
+import static com.ryanpmartz.lox.TokenType.RIGHT_BRACE;
+import static com.ryanpmartz.lox.TokenType.RIGHT_PAREN;
+import static com.ryanpmartz.lox.TokenType.SEMICOLON;
+import static com.ryanpmartz.lox.TokenType.STAR;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +29,7 @@ public class Scanner {
 
 	List<Token> scanTokens() {
 		while (!isAtEnd()) {
-			// we are at beggining of next lexeme
+			// we are at beginning of next lexeme
 			scanToken();
 		}
 
@@ -33,6 +43,52 @@ public class Scanner {
 	}
 
 	private void scanToken() {
+		char c = advance();
+		switch (c) {
+			case '(':
+				addToken(LEFT_PAREN);
+				break;
+			case ')':
+				addToken(RIGHT_PAREN);
+				break;
+			case '{':
+				addToken(LEFT_BRACE);
+				break;
+			case '}':
+				addToken(RIGHT_BRACE);
+				break;
+			case ',':
+				addToken(COMMA);
+				break;
+			case '.':
+				addToken(DOT);
+				break;
+			case '-':
+				addToken(MINUS);
+				break;
+			case '+':
+				addToken(PLUS);
+				break;
+			case ';':
+				addToken(SEMICOLON);
+				break;
+			case '*':
+				addToken(STAR);
+				break;
+		}
+	}
 
+	private char advance() {
+		current++;
+		return source.charAt(current - 1);
+	}
+
+	private void addToken(TokenType type) {
+		addToken(type, null);
+	}
+
+	private void addToken(TokenType type, Object literal) {
+		String text = source.substring(start, current);
+		tokens.add(new Token(type, text, literal, line));
 	}
 }
