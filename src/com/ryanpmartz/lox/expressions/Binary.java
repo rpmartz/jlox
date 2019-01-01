61f4bab46c73e35887bfc881ea328c2d031f8046
Ryan Martz <martzrp@gmail.com>
2019-01-01 09:10:29 -0500

    add abstract expression class and Binary class

---
+++ src/com/ryanpmartz/lox/expressions/Binary.java
@@ -0,0 +1,17 @@
+package com.ryanpmartz.lox.expressions;
+
+import com.ryanpmartz.lox.Token;
+
+public class Binary extends Expression {
+
+	final Expression left;
+	final Token operator;
+	final Expression right;
+
+	Binary(Expression left, Token operator, Expression right) {
+		this.left = left;
+		this.operator = operator;
+		this.right = right;
+	}
+
+}
---
+++ src/com/ryanpmartz/lox/expressions/Expression.java
@@ -0,0 +1,5 @@
+package com.ryanpmartz.lox.expressions;
+
+abstract class Expression {
+
+}
