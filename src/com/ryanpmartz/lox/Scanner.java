419eeebf9262563eda8c14a1ba0fc3fa5bca3980
Ryan Martz <martzrp@gmail.com>
2019-09-29 21:34:07 -0400

    add scanner class

---
+++ src/com/ryanpmartz/lox/Scanner.java
@@ -0,0 +1,14 @@
+package com.ryanpmartz.lox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+
+	private final String source;
+	private final List<Token> tokens = new ArrayList<>();
+
+	public Scanner(String source) {
+		this.source = source;
+	}
+}
