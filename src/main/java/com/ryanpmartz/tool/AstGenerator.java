44e94ea6628fa37d3190f20aa799f80d5823c01a
Ryan Martz <martzrp@gmail.com>
2019-10-05 11:33:19 -0400

    add visitor interface as nested interface in generated Expr class

--- src/main/java/com/ryanpmartz/tool/AstGenerator.java
+++ src/main/java/com/ryanpmartz/tool/AstGenerator.java
@@ -35,6 +35,8 @@ public class AstGenerator {
 		writer.println();
 		writer.println("abstract class " + baseName + "{ ");
 
+		defineVisitor(writer, baseName, types);
+
 		// The AST classes
 		for (String type : types) {
 			String className = type.split(":")[0].trim();
@@ -45,6 +47,19 @@ public class AstGenerator {
 		writer.println("}");
 		writer.close();
 
+	}
+
+	private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+		writer.println(" interface Visitor<R> {");
+
+		for (String type : types) {
+			String typeName = type.split(":")[0].trim();
+			writer.println("    R visit" + typeName + baseName + "("
+					+ typeName + " " + baseName.toLowerCase() + ");"
+			);
+		}
+
+		writer.println("   }");
 	}
 
 	private static void defineType(
