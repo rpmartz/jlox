2650d4decc706f912979dbfed700b42e5a8a246e
Ryan Martz <martzrp@gmail.com>
2019-10-29 07:32:51 -0400

    another test for a comparison

--- src/test/java/com/ryanpmartz/lox/ScannerTest.java
+++ src/test/java/com/ryanpmartz/lox/ScannerTest.java
@@ -2,6 +2,7 @@ package com.ryanpmartz.lox;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
@@ -51,6 +52,27 @@ class ScannerTest {
 		Token token = tokens.get(0);
 
 		assertEquals(token.type, TokenType.BANG_EQUAL);
+	}
+
+	@Test
+	public void testInvalidComparison() {
+		String source = "\"a\" > 5";
+		List<Token> tokens = new Scanner(source).scanTokens();
+
+		Token firstToken = tokens.get(0);
+		assertEquals(TokenType.STRING, firstToken.type);
+		assertEquals("\"a\"", firstToken.lexeme);
+		assertEquals("a", firstToken.literal);
+
+		Token secondToken = tokens.get(1);
+		assertEquals(TokenType.GREATER, secondToken.type);
+		assertEquals(">", secondToken.lexeme);
+		assertNull(secondToken.literal);
+
+		Token thirdToken = tokens.get(2);
+		assertEquals(TokenType.NUMBER, thirdToken.type);
+		assertEquals("5", thirdToken.lexeme);
+		assertEquals(5.0, thirdToken.literal);
 	}
 
 }
