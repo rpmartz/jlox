72b516b546c092d2967e376bfe03f17c0b9aaf4a
Ryan Martz <martzrp@gmail.com>
2020-01-20 19:08:53 -0500

    metaprogram the Stmt class

--- src/main/java/com/ryanpmartz/tool/AstGenerator.java
+++ src/main/java/com/ryanpmartz/tool/AstGenerator.java
@@ -23,6 +23,11 @@ public class AstGenerator {
 				"Unary    : Token operator, Expr right"
 		));
 
+		defineAst(outputDir, "Stmt", Arrays.asList(
+				"Expression : Expr expression",
+				"Print      : Expr expression"
+		));
+
 	}
 
 	private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
