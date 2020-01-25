c3c226737babfcf92024e63961d907a4e15dd771
Ryan Martz <martzrp@gmail.com>
2020-01-25 16:40:25 -0500

    updated generated classes for expression and statement

--- src/main/java/com/ryanpmartz/lox/Stmt.java
+++ src/main/java/com/ryanpmartz/lox/Stmt.java
@@ -5,6 +5,8 @@ abstract class Stmt {
         R visitExpressionStmt(Expression stmt);
 
         R visitPrintStmt(Print stmt);
+
+        R visitVarStmt(Var stmt);
     }
 
     static class Expression extends Stmt {
@@ -29,6 +31,20 @@ abstract class Stmt {
         }
 
         final Expr expression;
+    }
+
+    static class Var extends Stmt {
+        Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
+        }
+
+        final Token name;
+        final Expr initializer;
     }
 
     abstract <R> R accept(Visitor<R> visitor);
