c2f3091e2f5a24e8ae33facf919cf87edfdef102
Ryan Martz <martzrp@gmail.com>
2020-01-26 10:19:07 -0500

    add assignment() method in Parser

--- src/main/java/com/ryanpmartz/lox/Parser.java
+++ src/main/java/com/ryanpmartz/lox/Parser.java
@@ -76,7 +76,25 @@ public class Parser {
 
 	// when the body of the rule contains a nonterminal - a referrence to another rule, we call that rule's method
 	private Expr expression() {
-		return equality();
+		return assignment();
+	}
+
+	private Expr assignment() {
+		Expr expr = equality();
+
+		if (match(EQUAL)) {
+			Token equals = previous();
+			Expr value = assignment();
+
+			if (expr instanceof Expr.Variable) {
+				Token name = ((Expr.Variable) expr).name;
+				return new Expr.Assign(name, value);
+			}
+
+			error(equals, "Invalid assignment target.");
+		}
+
+		return expr;
 	}
 
 	private Stmt statement() {
