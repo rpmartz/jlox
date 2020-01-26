8c82d1f332a4eab6adcebbb24a83d1c4fd3c4954
Ryan Martz <martzrp@gmail.com>
2020-01-26 10:37:07 -0500

    add reference to environment to enable tree of envs

--- src/main/java/com/ryanpmartz/lox/Environment.java
+++ src/main/java/com/ryanpmartz/lox/Environment.java
@@ -5,7 +5,16 @@ import java.util.Map;
 
 public class Environment {
 
+	final Environment enclosing; // reference to parent
 	private final Map<String, Object> values = new HashMap<>();
+
+	public Environment() {
+		enclosing = null;
+	}
+
+	Environment(Environment env) {
+		this.enclosing = env;
+	}
 
 	public void define(String name, Object value) {
 		// note that there's no check whether variable here is already defined,
