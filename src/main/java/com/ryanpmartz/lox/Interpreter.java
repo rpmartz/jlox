5ce69dcf0d8d9ea3590707285d513a0ba995c1a0
Ryan Martz <martzrp@gmail.com>
2020-01-25 21:42:05 -0500

    add visitor implemntations for variable statements and expressions

--- src/main/java/com/ryanpmartz/lox/Interpreter.java
+++ src/main/java/com/ryanpmartz/lox/Interpreter.java
@@ -133,6 +133,23 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		return null;
 	}
 
+	@Override
+	public Object visitVariableExpr(Expr.Variable expr) {
+		return environment.get(expr.name);
+	}
+
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+		Object value = null;
+		if (stmt.initializer != null) {
+			value = evaluate(stmt.initializer);
+		}
+
+		environment.define(stmt.name.lexeme, value);
+
+		return null;
+	}
+
 	private boolean isTruthy(Object object) {
 		if (object == null) {
 			return false;
