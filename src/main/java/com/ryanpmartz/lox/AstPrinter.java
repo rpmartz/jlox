9a589b9ff8cc5683d5f2282c50e61c014bbe897b
Ryan Martz <martzrp@gmail.com>
2020-01-25 21:49:57 -0500

    add stub implementations in AstPrinter

--- src/main/java/com/ryanpmartz/lox/AstPrinter.java
+++ src/main/java/com/ryanpmartz/lox/AstPrinter.java
@@ -42,6 +42,11 @@ public class AstPrinter implements Expr.Visitor<String> {
 		return parenthesize(expr.operator.lexeme, expr.right);
 	}
 
+	@Override
+	public String visitVariableExpr(Expr.Variable expr) {
+		return null;
+	}
+
 	private String parenthesize(String name, Expr... exprs) {
 		StringBuilder sb = new StringBuilder();
 
