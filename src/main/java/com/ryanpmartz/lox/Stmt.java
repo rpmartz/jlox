c70ed4e471f177dab390ff7dcbfea21c4c8dd5fc
Ryan Martz <martzrp@gmail.com>
2020-01-26 21:20:45 -0500

    add a block to the ast generator

--- src/main/java/com/ryanpmartz/lox/Stmt.java
+++ src/main/java/com/ryanpmartz/lox/Stmt.java
@@ -1,51 +1,67 @@
 package com.ryanpmartz.lox;
 
+import java.util.List;
+
 abstract class Stmt {
-    interface Visitor<R> {
-        R visitExpressionStmt(Expression stmt);
+	interface Visitor<R> {
+		R visitBlockStmt(Block stmt);
 
-        R visitPrintStmt(Print stmt);
+		R visitExpressionStmt(Expression stmt);
 
-        R visitVarStmt(Var stmt);
-    }
+		R visitPrintStmt(Print stmt);
 
-    static class Expression extends Stmt {
-        Expression(Expr expression) {
-            this.expression = expression;
-        }
+		R visitVarStmt(Var stmt);
+	}
 
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitExpressionStmt(this);
-        }
+	static class Block extends Stmt {
+		Block(List<Stmt> statements) {
+			this.statements = statements;
+		}
 
-        final Expr expression;
-    }
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitBlockStmt(this);
+		}
 
-    static class Print extends Stmt {
-        Print(Expr expression) {
-            this.expression = expression;
-        }
+		final List<Stmt> statements;
+	}
 
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitPrintStmt(this);
-        }
+	static class Expression extends Stmt {
+		Expression(Expr expression) {
+			this.expression = expression;
+		}
 
-        final Expr expression;
-    }
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitExpressionStmt(this);
+		}
 
-    static class Var extends Stmt {
-        Var(Token name, Expr initializer) {
-            this.name = name;
-            this.initializer = initializer;
-        }
+		final Expr expression;
+	}
 
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitVarStmt(this);
-        }
+	static class Print extends Stmt {
+		Print(Expr expression) {
+			this.expression = expression;
+		}
 
-        final Token name;
-        final Expr initializer;
-    }
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitPrintStmt(this);
+		}
 
-    abstract <R> R accept(Visitor<R> visitor);
+		final Expr expression;
+	}
+
+	static class Var extends Stmt {
+		Var(Token name, Expr initializer) {
+			this.name = name;
+			this.initializer = initializer;
+		}
+
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitVarStmt(this);
+		}
+
+		final Token name;
+		final Expr initializer;
+	}
+
+	abstract <R> R accept(Visitor<R> visitor);
 }
