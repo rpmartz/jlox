064e288c065c8dab38d018774fe2da4902c2f240
Ryan Martz <martzrp@gmail.com>
2020-01-28 07:19:58 -0500

    add if statement to interprepter

--- src/main/java/com/ryanpmartz/lox/Interpreter.java
+++ src/main/java/com/ryanpmartz/lox/Interpreter.java
@@ -165,6 +165,17 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		return null;
 	}
 
+	@Override
+	public Void visitIfStmt(Stmt.If stmt) {
+		if (isTruthy(evaluate(stmt.condition))) {
+			execute(stmt.thenBranch);
+		} else if (stmt.elseBranch != null) {
+			execute(stmt.elseBranch);
+		}
+
+		return null;
+	}
+
 	private void executeBlock(List<Stmt> statements, Environment environment) {
 		Environment previous = this.environment;
 		try {
