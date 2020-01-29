d3db072cbde92516d602dcc93d50904e140fc198
Ryan Martz <martzrp@gmail.com>
2020-01-29 07:48:08 -0500

    implement logical expression in interpreter

--- src/main/java/com/ryanpmartz/lox/Interpreter.java
+++ src/main/java/com/ryanpmartz/lox/Interpreter.java
@@ -176,6 +176,22 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		return null;
 	}
 
+	@Override
+	public Object visitLogicalExpr(Expr.Logical expr) {
+		Object left = evaluate(expr.left);
+
+		if (expr.operator.type == TokenType.OR) {
+			if (isTruthy(left)) return left;
+		} else {
+			if (!isTruthy(left)) {
+				if (!isTruthy(left)) return left;
+				return left;
+			}
+		}
+
+		return evaluate(expr.right);
+	}
+
 	private void executeBlock(List<Stmt> statements, Environment environment) {
 		Environment previous = this.environment;
 		try {
