43aab9317a4eccd524c5523d055f45575b21c6fe
Ryan Martz <martzrp@gmail.com>
2020-01-30 08:01:17 -0500

    add parsing rules for call in the Parser

--- src/main/java/com/ryanpmartz/lox/Parser.java
+++ src/main/java/com/ryanpmartz/lox/Parser.java
@@ -3,6 +3,7 @@ package com.ryanpmartz.lox;
 import static com.ryanpmartz.lox.TokenType.AND;
 import static com.ryanpmartz.lox.TokenType.BANG;
 import static com.ryanpmartz.lox.TokenType.BANG_EQUAL;
+import static com.ryanpmartz.lox.TokenType.COMMA;
 import static com.ryanpmartz.lox.TokenType.ELSE;
 import static com.ryanpmartz.lox.TokenType.EOF;
 import static com.ryanpmartz.lox.TokenType.EQUAL;
@@ -306,7 +307,35 @@ public class Parser {
 			return new Expr.Unary(operator, right);
 		}
 
-		return primary();
+		return call();
+	}
+
+	private Expr call() {
+		Expr expr = primary();
+
+		while (true) {
+			if (match(LEFT_PAREN)) {
+				expr = finishCall(expr);
+			} else {
+				break;
+			}
+		}
+
+		return expr;
+	}
+
+	// roughly the arguments grammar rule translated to code but with zero argument case
+	private Expr finishCall(Expr callee) {
+		List<Expr> arguments = new ArrayList<>();
+		if (!check(RIGHT_PAREN)) {
+			do {
+				arguments.add(expression());
+			} while (match(COMMA));
+		}
+
+		Token paren = consume(RIGHT_PAREN, "Expect ')' after arguments.");
+
+		return new Expr.Call(callee, paren, arguments);
 	}
 
 	private Expr primary() {
