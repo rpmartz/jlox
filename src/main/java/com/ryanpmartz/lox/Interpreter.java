fdccc690f478b83e142a9c2af1c6839bc4d74834
Ryan Martz <martzrp@gmail.com>
2020-02-04 08:10:53 -0500

    implement visiting void functions

--- src/main/java/com/ryanpmartz/lox/Interpreter.java
+++ src/main/java/com/ryanpmartz/lox/Interpreter.java
@@ -251,6 +251,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		return function.call(this, arguments);
 	}
 
+	@Override
+	public Void visitFunctionStmt(Stmt.Function stmt) {
+		LoxFunction function = new LoxFunction(stmt);
+		environment.define(stmt.name.lexeme, function);
+
+		return null;
+	}
+
 	public void executeBlock(List<Stmt> statements, Environment environment) {
 		Environment previous = this.environment;
 		try {
