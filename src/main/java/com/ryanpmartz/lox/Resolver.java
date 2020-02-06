28c77e1fd5bbf9e9634bfa6048a99a63d42e97e3
Ryan Martz <martzrp@gmail.com>
2020-02-06 08:20:28 -0500

    implement declaration and definition for variables

--- src/main/java/com/ryanpmartz/lox/Resolver.java
+++ src/main/java/com/ryanpmartz/lox/Resolver.java
@@ -22,6 +22,18 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		return null;
 	}
 
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+		declare(stmt.name);
+		if (stmt.initializer != null) {
+			resolve(stmt.initializer);
+		}
+
+		define(stmt.name);
+
+		return null;
+	}
+
 	private void resolve(List<Stmt> statements) {
 		for (Stmt statement : statements) {
 			resolve(statement);
@@ -32,11 +44,31 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		stmt.accept(this);
 	}
 
+	private void resolve(Expr expr) {
+		expr.accept(this);
+	}
+
 	private void beginScope() {
-		scopes.push(new HashMap<String, Boolean>());
+		scopes.push(new HashMap<>());
 	}
 
 	private void endScope() {
 		scopes.pop();
+	}
+
+	private void declare(Token name) {
+		if (scopes.isEmpty()) {
+			return;
+		}
+
+		Map<String, Boolean> scope = scopes.peek();
+		scope.put(name.lexeme, false); // false means is declared but not initialized
+	}
+
+	private void define(Token name) {
+		if (scopes.isEmpty()) {
+			return;
+		}
+		scopes.peek().put(name.lexeme, true);
 	}
 }
